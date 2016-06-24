8d7db544937b22bb84e27fa1955996e57653e47b
haifahrul <haifahrul@gmail.com>
2016-06-24 23:02:09 +0700

    fix

--- config/web-local.php
+++ config/web-local.php
@@ -6,16 +6,27 @@
  * Time: 0:45
  */
 
-return [
+$config = [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=prod_db',
+            'dsn' => 'mysql:host=localhost;dbname=yii2-basic-afs',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
         ],
 
-        // ... other components ...
+        // ... other components
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
